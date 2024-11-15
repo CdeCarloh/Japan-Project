f6824bdafc259be160a72991a34b83b238c5503f
CdeCarloh <carlosperezsantana04@gmail.com>
2024-11-15 16:37:44 +0000

    more changes

--- js/validate.js
+++ js/validate.js
@@ -4,65 +4,51 @@ function listenForValidation() {
 }
 
 function validateFormContact(e) {
-  e.preventDefault();
 
-  const name = e.target.name.value;
-  const surname = e.target.surname.value;
-  const email = e.target.email.value;
-  const message = e.target.message.value;
+  const NAME = e.target.name.value;
+  const SURNAME = e.target.surname.value;
+  const EMAIL = e.target.email.value;
+  const MESSAGE = e.target.message.value;
 
   let valid = true;
 
-  if (!name) {
+  if (!NAME) {
     document.getElementById("form-name").style.visibility = "visible";
     valid = false;
   } else {
     document.getElementById("form-name").style.visibility = "hidden";
   }
 
-  if (!surname) {
+  if (!SURNAME) {
     document.getElementById("form-surname").style.visibility = "visible";
     valid = false;
   } else {
     document.getElementById("form-surname").style.visibility = "hidden";
   }
 
-  if (!email) {
+  if (!EMAIL) {
     document.getElementById("form-email").style.visibility = "visible";
-    document.getElementById("form-email-invalid").style.visibility = "hidden";
     valid = false;
-  } else if (!validateEmail(email)) {
-    document.getElementById("form-email-invalid").style.visibility = "visible";
-    document.getElementById("form-email").style.visibility = "hidden";
-    valid = false;
-  } else {
-    document.getElementById("form-email").style.visibility = "hidden";
-    document.getElementById("form-email-invalid").style.visibility = "hidden";
-  }
+  } else document.getElementById("form-email").style.visibility = "hidden";
 
-  if (!message) {
+  if (!MESSAGE) {
     document.getElementById("form-message").style.visibility = "visible";
     valid = false;
+  } else document.getElementById("form-message").style.visibility = "hidden";
+
+
+  if (!valid) {
+    e.preventDefault();
   } else {
-    document.getElementById("form-message").style.visibility = "hidden";
-  }
-
-  if (valid) {
-    saveData(name, surname, email, message);
-    window.location.href = "show-data.html"
+    saveData(NAME, SURNAME, EMAIL, MESSAGE);
   }
 }
 
-function saveData(name, surname, email, message) {
-  localStorage.setItem("name", name);
-  localStorage.setItem("surname", surname);
-  localStorage.setItem("email", email);
-  localStorage.setItem("message", message);
-}
-
-function validateEmail(email) {
-  const validEmail = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
-  return validEmail.test(email);
+function saveData(NAME, SURNAME, EMAIL, MESSAGE) {
+  localStorage.setItem("name", NAME);
+  localStorage.setItem("surname", SURNAME);
+  localStorage.setItem("email", EMAIL);
+  localStorage.setItem("message", MESSAGE);
 }
 
 listenForValidation();
